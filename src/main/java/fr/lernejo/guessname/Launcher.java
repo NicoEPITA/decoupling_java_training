3af5788c31bd7e3f4d2efc479fed32630b747a2c
Nicolas NATANEK <nicolas.natanek@epita.fr>
2023-04-05 18:57:32 +0200

    Part 3

--- src/main/java/fr/lernejo/guessname/Launcher.java
+++ src/main/java/fr/lernejo/guessname/Launcher.java
@@ -3,11 +3,13 @@ package fr.lernejo.guessname;
 import java.security.SecureRandom;
 
 public class Launcher {
+
     public static void main(String[] args) {
         SecureRandom random = new SecureRandom();
-        int numberToGuess = random.nextInt(100);
-        Simulation sim = new Simulation(new HumanPlayer());
-        sim.initialize(numberToGuess);
-        sim.loopUntilPlayerSucceed();
+        long randomNumber = random.nextInt(100);
+        Player player = new HumanPlayer();
+        Simulation simulation = new Simulation(player);
+        simulation.initialize(randomNumber);
+        simulation.loopUntilPlayerSucceed();
     }
 }
--- src/main/java/fr/lernejo/guessname/Simulation.java
+++ src/main/java/fr/lernejo/guessname/Simulation.java
@@ -1,45 +1,47 @@
 package fr.lernejo.guessname;
 import fr.lernejo.logger.Logger;
 import fr.lernejo.logger.LoggerFactory;
-
+import java.util.Scanner;
 
 public class Simulation {
 
     private final Logger logger = LoggerFactory.getLogger("simulation");
     private final Player player;
     private long numberToGuess;
+    private Scanner scanner;
+
     public Simulation(Player player) {
         this.player = player;
+        this.numberToGuess = 0;
+        this.scanner = new Scanner(System.in);
     }
 
     public void initialize(long numberToGuess) {
-        //implement me
         this.numberToGuess = numberToGuess;
     }
 
     /**
-     * @return true if the player have guessed the right number
-     */
 
-    boolean nextRound() {
-        long guess = player.askNextGuess();
-        if (guess == numberToGuess) {
-            logger.log("You guessed the right number");
+     @return true if the player have guessed the right number
+     */
+    private boolean nextRound() {
+        logger.log("Enter a number:");
+        long playerGuess = scanner.nextLong();
+        if (playerGuess == numberToGuess) {
+            logger.log("Congratulations! You won!");
             return true;
+        } else if (playerGuess < numberToGuess) {
+            logger.log("The number to guess is greater than " + playerGuess);
+        } else {
+            logger.log("The number to guess is smaller than " + playerGuess);
         }
-        else if (guess > numberToGuess) {
-            logger.log("The number is lower than your guess");
-            return false;
-        }
-        else {
-            logger.log("The number is greater than your guess");
-            return false;
-        }
+        return false;
     }
 
     public void loopUntilPlayerSucceed() {
-        while (nextRound()) {
-            nextRound();
+        boolean success = false;
+        while (!success) {
+            success = nextRound();
         }
     }
 }
