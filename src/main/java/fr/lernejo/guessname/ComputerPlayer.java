31fe2cbd7952ee27d463c247aa1d86caa6633ebd
Nicolas NATANEK <nicolas.natanek@epita.fr>
2023-04-06 17:24:22 +0200

    Part 4 second try

--- src/main/java/fr/lernejo/guessname/ComputerPlayer.java
+++ src/main/java/fr/lernejo/guessname/ComputerPlayer.java
@@ -1,29 +1,30 @@
 package fr.lernejo.guessname;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 public class ComputerPlayer implements Player {
-    private long nextGuess;
 
-    public ComputerPlayer() {
-        this.nextGuess = ThreadLocalRandom.current().nextLong(1, 101);
+    private long min = 0;
+    private long max = 100;
+    private long lastGuess = -1;
+
+    public int getNumberToGuess() {
+        return (int) ((min + max) / 2);
+    }
+
+    public void updateWithResult(boolean result) {
+        if (result) {
+            max = lastGuess;
+        } else {
+            min = lastGuess + 1;
+        }
     }
 
     @Override
     public long askNextGuess() {
-        return nextGuess();
+        return 0;
     }
 
     @Override
     public void respond(boolean lowerOrGreater) {
-        if (lowerOrGreater) {
-            nextGuess = ThreadLocalRandom.current().nextLong(1, nextGuess);
-        } else {
-            nextGuess = ThreadLocalRandom.current().nextLong(nextGuess + 1, 101);
-        }
-    }
 
-    private long nextGuess() {
-        return (nextGuess + 1 + ThreadLocalRandom.current().nextLong(1, 101)) / 2;
     }
 }
--- src/main/java/fr/lernejo/guessname/Launcher.java
+++ src/main/java/fr/lernejo/guessname/Launcher.java
@@ -3,37 +3,29 @@ package fr.lernejo.guessname;
 import java.security.SecureRandom;
 
 public class Launcher {
-
     public static void main(String[] args) {
-        if (args.length == 1 && args[0].equals("-interactive")) {
-            launchInteractiveGame();
-        } else if (args.length == 2 && args[0].equals("-auto") && isNumeric(args[1])) {
-            launchAutoGame(Long.parseLong(args[1]));
+        if (args.length == 0) {
+            System.out.println("Usage: Launcher -interactive | -auto <number>");
+        } else if (args[0].equals("-interactive")) {
+            SecureRandom random = new SecureRandom();
+            long randomNumber = random.nextInt(100);
+            Player player = new HumanPlayer();
+            Simulation simulation = new Simulation(player);
+            simulation.initialize(randomNumber);
+            simulation.loopUntilPlayerSucceed(1000);
+        } else if (args[0].equals("-auto") && args.length == 2) {
+            long randomNumber = Long.parseLong(args[1]);
+            Player player = new ComputerPlayer();
+            Simulation simulation = new Simulation(player);
+            simulation.initialize(randomNumber);
+            simulation.loopUntilPlayerSucceed(1000);
         } else {
-            System.out.println("Usage:");
-            System.out.println("java Launcher -interactive (play the game interactively with a HumanPlayer)");
-            System.out.println("java Launcher -auto <number> (play the game automatically with a ComputerPlayer, guessing the given number)");
+            System.out.println("Usage: Launcher -interactive | -auto <number>");
         }
     }
-
-    private static boolean isNumeric(String str) {
-        return str.matches("-?\\d+(\\.\\d+)?");
-    }
-
-    private static void launchInteractiveGame() {
-        SecureRandom random = new SecureRandom();
-        long randomNumber = random.nextInt(100);
-        Player player = new HumanPlayer();
-        Simulation simulation = new Simulation(player);
-        simulation.initialize(randomNumber);
-        simulation.loopUntilPlayerSucceed(1000);
-    }
-
-    private static void launchAutoGame(int numberToGuess) {
-        Player player = (Player) new ComputerPlayer();
-        Simulation simulation = new Simulation(player);
-        simulation.initialize(numberToGuess);
-        simulation.loopUntilPlayerSucceed(1000);
-    }
-
 }
+
+
+
+
+
